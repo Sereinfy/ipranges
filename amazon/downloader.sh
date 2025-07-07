f0c0094d5a337523da6ca83c7dbd441ebd369ae4
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2025-07-07 10:09:57 +0800

    Update downloader.sh

--- amazon/downloader.sh
+++ amazon/downloader.sh
@@ -20,3 +20,15 @@ jq '.ipv6_prefixes[] | [.ipv6_prefix][] | select(. != null)' -r /tmp/amazon.json
 # sort & uniq
 sort -h /tmp/amazon-ipv4.txt | uniq > amazon/ipv4.txt
 sort -h /tmp/amazon-ipv6.txt | uniq > amazon/ipv6.txt
+
+
+# save ipv4 - only where service is CLOUDFRONT
+jq '.prefixes[] | select(.service == "CLOUDFRONT") | [.ip_prefix][] | select(. != null)' -r /tmp/amazon.json > /tmp/CLOUDFRONT-ipv4.txt
+
+# save ipv6 - only where service is CLOUDFRONT
+jq '.ipv6_prefixes[] | select(.service == "CLOUDFRONT") | [.ipv6_prefix][] | select(. != null)' -r /tmp/amazon.json > /tmp/CLOUDFRONT-ipv6.txt
+
+
+# sort & uniq
+sort -h /tmp/CLOUDFRONT-ipv4.txt | uniq > amazon/cloudfront_ipv4.txt
+sort -h /tmp/CLOUDFRONT-ipv6.txt | uniq > amazon/cloudfront_ipv6.txt
